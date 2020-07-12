32d667ed6372a3a9edc419949c6f335b921a4645
Aashish <ashishdahal490@gmail.com>
2020-07-12 09:06:16 +0545

    /get/posts

--- routes/khasi_category.js
+++ routes/khasi_category.js
@@ -31,7 +31,7 @@ var storage = multer.diskStorage({
 
 /*-----Handling Get requests----------*/
 
-router.get('/',(req,res,next)=>{
+router.get('/get',(req,res,next)=>{
     khasiModel.find()
     .select('_id title category short_description estimated_weight color daat owner_name address primary_contact_no secondary_contact_no age khasiImage')
     .exec()
@@ -54,7 +54,7 @@ router.get('/',(req,res,next)=>{
 /*-----Handling Post requests----------*/
 
 // router.post('/',upload.single('khasiImage'),checkAuth,(req,res,next)=>{
-    router.post('/',upload.single('khasiImage'),(req,res,next)=>{
+    router.post('/post',upload.single('khasiImage'),(req,res,next)=>{
     console.log(req.userData);
     const khasiLists=new khasiModel({
           title:req.body.title,
